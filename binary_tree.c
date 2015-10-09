152f3c7b6055e96b6a6f792e5896f8d108264aab
apolcyn <alexpolcyn@comcast.net>
2015-10-09 01:49:14 -0700

    whole thing is working

--- binary_tree.c
+++ binary_tree.c
@@ -4,7 +4,6 @@
 struct node {
    struct node *left;
    struct node *right;
-   struct node *parent;
    int val;
    int height;
 };
@@ -25,47 +24,71 @@ int diff_heights(struct node *root) {
    return height(root->right) - height(root->left);
 }
 
-struct node* addToTree(struct node *root, struct node *parent, struct node *new_node) {
-   struct node *new_root;
+// takes in a balanced tree, returns a balanced tree after the new node added to it
+struct node* addToTree(struct node *root, struct node *new_node) {
+   struct node *new_root = root;
 
    fprintf(stderr, "calling\n");
 
    if(root == NULL) {
-      new_node->parent = parent;
       return new_node;
    }
    else if(new_node->val > root->val) {
-      root->right = addToTree(root->right, root, new_node);
+      root->right = addToTree(root->right, new_node);
    }
    else {
-      root->left = addToTree(root->left, root, new_node);
+      root->left = addToTree(root->left, new_node);
    }
 
    if(diff_heights(root) > 1) {
       if(diff_heights(root->right) >= 0) {
 	 new_root = root->right;
-	 root->right = NULL;
-	 new_root->left = addToTree(root, new_root, new_root->left);
-	 new_root->parent = parent;
+	 root->right = new_root->left;
+	 new_root->left = root;
       }
       else {
-	 printf("shouldn't be trying to add a new number here\n");
-	 exit(-1);
+	 new_root = root->right->left;
+	 root->right->left = new_root->right;
+	 new_root->right = root->right;
+	 root->right = new_root->left;
+	 new_root->left = root;
       }
    }
    else if(diff_heights(root) < -1) {
-      printf("shouln't be here\n");
-      exit(-1);
       if(diff_heights(root->left) <= 0) {
-         // easy
+         new_root = root->left;
+	 root->left = new_root->right;
+	 new_root->right = root;
       }
       else {
-         // hard
+	 new_root = root->left->right;
+	 root->left->right = new_root->left;
+	 new_root->left = root->left;
+	 root->left = new_root->right;
+	 new_root->right = root;
       }
    }
 
-   new_root->height = max(height(new_root->right), height(new_root->left));
+   new_root->height = max(height(new_root->right), height(new_root->left)) + 1;
    return new_root;
+}
+
+void print_spaces(int count) {
+   while(count--)
+      printf(" ");
+}
+
+void graphic(struct node *tree, int indent) {
+   if(tree) {
+      print_spaces(indent);
+      printf("left subtree:\n");  
+      graphic(tree->left, indent + 3);
+      print_spaces(indent);
+      printf("%d\n", tree->val);  
+      print_spaces(indent);
+      printf("right subtree:\n");  
+      graphic(tree->right, indent + 3);
+   }
 }
 
 void printInOrder(struct node *tree) {
@@ -81,16 +104,15 @@ int main(char **argv, int argc) {
    struct node *tree = NULL;
    struct node *temp = NULL;
    
-   while(1) {
+   while(scanf("%d\n", &val) && !feof(stdin)) {
       fprintf(stderr, "enter a num\n");
-      scanf("%d\n", &val);
-      if(val < 0)
-	 break;
       temp = calloc(1, sizeof(struct node));
       temp->val = val;
-      tree = addToTree(tree, NULL, temp);
+      temp->height = 0;
+      tree = addToTree(tree, temp);
    }
 
    printInOrder(tree);
+   graphic(tree, 0);
    return 0;
 }
